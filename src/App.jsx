7d2b9b033a92771c529eb7974f1f12e6d06943a6
greg-argulla <greg.argulla@gmail.com>
2023-06-28 23:13:14 +0800

    fix: remove ugly white scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1124,6 +1124,7 @@ function App() {
       >
         <div
           id="chatbox"
+          className="scrollable-container"
           style={{
             backgroundColor: "#333",
             padding: 10,
